7ccd316370cb2d54fb66b4ccf38cb5444a69b285
Antony Kamp <tony.kamp@web.de>
2022-11-01 08:40:44 +0100

    setup index page

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -7,4 +7,13 @@ describe "Show author page", type: :feature do
         expect(page).to have_text("Turing")
         expect(page).to have_text("http://wikipedia.org/Alan_Turing")
     end
+
+    it "Should display alan turing on index page" do
+        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        author.save
+        visit authors_path
+        expect(page).to have_text("Alan")
+        expect(page).to have_text("Turing")
+        expect(page).to have_text("http://wikipedia.org/Alan_Turing")
+    end
 end
