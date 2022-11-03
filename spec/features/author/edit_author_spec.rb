004070d573fec5452b0397fcf368a9a0e8ef279e
Antony Kamp <tony.kamp@web.de>
2022-11-03 11:20:59 +0100

    check for error

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -8,4 +8,14 @@ describe "Edit author page", type: :feature do
         author.reload
         expect(author.name).to eq("Tom Turing")
     end
+    it "shoudl show error" do
+        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        author.save
+        visit edit_author_path(author)
+        page.fill_in "author[first_name]",	with: "" 
+        page.fill_in "author[last_name]",	with: "" 
+        page.fill_in "author[homepage]",	 with: "" 
+        find("input[type='submit']").click
+        expect(page).to have_text("Error")
+     end
 end
